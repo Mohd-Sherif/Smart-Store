0a52141f861aa16d1f76c73188f27fe05ba295ae
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-06-18 19:38:59 +0300

    Update main.c

--- Billing Module/Billing Module/main.c
+++ Billing Module/Billing Module/main.c
@@ -16,12 +16,42 @@
 #define ITEMS_COUNT 10
 #define BAR_CODE_LENGTH 8
 
+
+volatile unsigned char receivedProducts[ITEMS_COUNT][BAR_CODE_LENGTH], received = 0, found = 0;
+volatile unsigned int totalCost;
 int main(void)
 {
     USART_vinit(9600);
     LCD_vinit();
+	LCD_vsend_string(" Welcome To Our ");
+	LCD_vmove_cursor(2,1);
+	LCD_vsend_string("   Smart Store  ");
     while (1) 
     {
+		if(USART_u8receive_data() == START_MESSAGE){
+			totalCost = 0;
+			LCD_vCLR_screen();
+			LCD_vsend_string("Reading Products");
+			LCD_vmove_cursor(2,1);
+			LCD_vsend_string("Please Wait...");
+			receiving();
+			LCD_vCLR_screen();
+			LCD_vsend_string("Calculating cost");
+			LCD_vmove_cursor(2,1);
+			LCD_vsend_string("Please Wait...");
+			_delay_ms(500);
+			calculatingCost();
+			printCost();
+			LCD_vCLR_screen();
+			LCD_vsend_string("Thanks for");
+			LCD_vmove_cursor(2,1);
+			LCD_vsend_string("your visit");
+			_delay_ms(500);
+			LCD_vCLR_screen();
+			LCD_vsend_string(" Welcome To Our ");
+			LCD_vmove_cursor(2,1);
+			LCD_vsend_string("   Smart Store  ");
+		}
     }
 }
 
